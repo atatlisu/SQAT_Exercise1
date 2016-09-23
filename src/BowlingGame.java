aff1fcfa2a9a5adf1eefd8ab47d3768ed14ad996
somename <someemail>
2016-09-23 15:23:30 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 //Finish time:
@@ -8,23 +9,25 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	
+
 	public BowlingGame(){}
 	
 	// adds a frame to the game
 	public void addFrame(Frame frame){
 		//to be implemented
-		Frame frame = new Frame(frames.get(0));
+	frames.add(frame);
 	}
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+		frames.add(new Frame(firstThrow,secondThrow));
 	}
 	
 	// Returns the game score
 	public int score(){
 		//to be implemented: should return game score 
-		return 0;
+	
+			return 0;
+		}
+		
 	}
-}
